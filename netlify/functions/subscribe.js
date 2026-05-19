a3f688a757242fd4baf97879ffb271dc02b68d00
Alhena667 <valentinabrionesgutierrez775@gmail.com>
2026-05-19 16:43:43 -0400

    Update subscribe.js

--- netlify/functions/subscribe.js
+++ netlify/functions/subscribe.js
@@ -19,12 +19,15 @@ exports.handler = async function(event) {
 
     const result = await response.json();
 
+    console.log("Brevo response:", result);
+
     return {
       statusCode: response.status,
       body: JSON.stringify(result)
     };
 
   } catch (error) {
+    console.log("ERROR:", error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message })
